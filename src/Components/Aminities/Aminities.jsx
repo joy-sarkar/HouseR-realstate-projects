0365f5195d66d095e7193a993153dc08d790026f
JOY <joy.contact.21@gmail.comm>
2023-10-15 15:57:03 +0600

    call to action added

--- src/Components/Aminities/Aminities.jsx
+++ src/Components/Aminities/Aminities.jsx
@@ -1,19 +1,31 @@
 import { Amenities } from "../index";
-import {BsArrowRight} from 'react-icons/bs'
+import { BsArrowRight } from "react-icons/bs";
 const Aminities = () => {
   return (
     <div>
       <section className="bg-[#f2f6f7] md:px-14">
-        <div><h1 className="text-[3.5rem] font-black text-center text-slate-950 pt-12">Building Amenities</h1></div>
+        <div>
+          <h1 className="text-[3.5rem] font-black text-center text-slate-950 pt-12">
+            Building Amenities
+          </h1>
+        </div>
         <div className="grid md:grid-cols-3 md:gap-5 place-items-center justify-items-center md:py-16 ">
           {Amenities.map((item) => (
-            <div key={item.id} className="md:py-24 md:px-[7.5rem] rounded shadow-2xl flex justify-center items-center duration-500 flex-col group relative hover:bg-[#ff593cad]">
+            <div
+              key={item.id}
+              className="md:py-24 md:px-[7.5rem] rounded shadow-2xl flex justify-center items-center duration-500 flex-col group relative hover:bg-[#ff593cad]"
+            >
               <div className="bg-[#ff593c23] duration-500 group-hover:bg-slate-200 w-24 h-24 flex justify-center items-center rounded-full">
                 <item.icon color="#ff5a3c" size={36} />
               </div>
-              <p className="from-neutral-800 text-slate-950 font-bold text-lg pt-4">{item.title}</p>
+              <p className="from-neutral-800 text-slate-950 font-bold text-lg pt-4">
+                {item.title}
+              </p>
               <div className="w-14 h-14  rounded-full bg-[#f2f6f7] shadow-lg absolute bottom-[-1rem] flex justify-center items-center">
-              <BsArrowRight className="" size={32}/>
+                <BsArrowRight
+                  className="cursor-pointer  duration-500  group-hover:text-[#ff5a3c]"
+                  size={32}
+                />
               </div>
             </div>
           ))}
--- src/Components/CTA/CTA.jsx
+++ src/Components/CTA/CTA.jsx
@@ -1,8 +1,22 @@
+import { BsArrowRight } from "react-icons/bs";
+import { ctaData } from "../index";
 const CTA = () => {
   return (
-    <div>
-      <h1>CTA Section</h1>
-    </div>
+    <section className="lg:px-8 lg:py-4 bg-[#fdfdff]">
+      <div className="lg:mx-12 lg:flex justify-between bg-[#ff5a3c] p-12">
+        <div>
+          <h1 className=" text-[2rem] text-slate-100 font-extrabold">
+            {ctaData.title}
+          </h1>
+          <p className=" text-base text-slate-100 font-medium">
+            {ctaData.subtext}
+          </p>
+        </div>
+        <div>
+          <button className="px-12 py-6 text-slate-950 bg-slate-100">{ctaData.btnText} <BsArrowRight size={20} className="inline-block"/></button>
+        </div>
+      </div>
+    </section>
   );
 };
 
